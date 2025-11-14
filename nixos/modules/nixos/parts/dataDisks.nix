74d6f10fe3608023323ebff59bbd4fcfdc1b9a5f
Arnaud Lemaire <alemaire@norad.fr>
2025-11-14 15:35:24 +0100

    up

--- nixos/modules/nixos/parts/dataDisks.nix
+++ nixos/modules/nixos/parts/dataDisks.nix
@@ -12,15 +12,6 @@ let
   }) disks);
   crypttabText = lib.concatStringsSep "\n" (lib.mapAttrsToList (name: path: "${name}  ${path}          none luks") disks);
   hasDataDisks = disks != {};
-  # /data mounts moved from system definition; enabled only when data disks defined
-  mkBindData = from: to: {
-    name = "/data/${to}";
-    value = {
-      device = "${from}";
-      fsType = "none";
-      options = ["bind" "noauto"];
-    };
-  };
   mkMergeData = type: {
     name = "/data/${type}";
     value = {
@@ -29,21 +20,15 @@ let
       options = [ "ro" "minfreespace=50G" "category.create=msplfs" "noauto" ];
     };
   };
-  dataFileSystems = builtins.listToAttrs [
-    (mkBindData "/disks/hdd5" "download")
-    (mkBindData "/disks/hdd5/backup" "backup")
-    (mkBindData "/disks/nvme1/cache" "cache")
+  mergerfsFileSystems = builtins.listToAttrs [
     (mkMergeData "audio")
     (mkMergeData "video")
     (mkMergeData "image")
   ];
-  mountBindsService = {
-    systemd.services."mount-binds" = {
+  mountMergerfsService = {
+    systemd.services."mount-mergerfs" = {
       enable = true;
       script = ''
-        systemctl start data-download.mount
-        systemctl start data-backup.mount
-        systemctl start data-cache.mount
         systemctl start data-audio.mount
         systemctl start data-video.mount
         systemctl start data-image.mount
@@ -60,13 +45,13 @@ in {
       nvme1 = "/dev/disk/by-id/nvme-dsfsdfs00";
       hdd1 = "/dev/disk/by-id/ata-sdfdsQBJW8L1T";
     };
-    description = "Mount encrypted data files to /disks/{name}. Also provides /data binds & mergerfs mounts when any data disk is defined.";
+    description = "Mount encrypted data files to /disks/{name}. Also provides mergerfs mountsto data/* folders";
   };
 
   config = lib.mkMerge [
     { fileSystems = fileSystemsEntries; }
     (lib.mkIf (crypttabText != "") { environment.etc.crypttab.text = crypttabText + "\n"; })
-    (lib.mkIf hasDataDisks { fileSystems = dataFileSystems; })
-    (lib.mkIf hasDataDisks mountBindsService)
+    (lib.mkIf hasDataDisks { fileSystems = mergerfsFileSystems; })
+    (lib.mkIf hasDataDisks mountMergerfsService)
   ];
 }
