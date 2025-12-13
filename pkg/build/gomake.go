0d1f331b6d8caedb7a84139e4fc7c907e053bd88
Arnaud Lemaire <alemaire@norad.fr>
2025-12-13 12:25:49 +0100

    ci: do github release

--- pkg/build/gomake.go
+++ pkg/build/gomake.go
@@ -19,7 +19,7 @@ func main() {
 			},
 		}).
 		WithStep(&gomake.StepRelease{
-			GithubRelease: false,
+			GithubRelease: true,
 			OsArchRelease: []string{"linux-amd64", "darwin-amd64", "linux-arm64", "darwin-arm64"},
 			PostReleaseHook: func(release gomake.StepRelease) error {
 				//release.Version
