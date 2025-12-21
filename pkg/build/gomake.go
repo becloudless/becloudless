4b71813ca8d08acdf86928fb94f196dd4db43442
Arnaud Lemaire <agithub@lmr.fr>
2025-12-21 11:00:59 +0100

    feat(ci): do daily release (#72)

--- pkg/build/gomake.go
+++ pkg/build/gomake.go
@@ -7,7 +7,6 @@ import (
 	"strings"
 	"time"
 
-	"github.com/n0rad/go-erlog/errs"
 	"github.com/n0rad/gomake"
 )
 
@@ -16,15 +15,11 @@ func main() {
 		WithName("bcl").
 		WithVersionFunc(func() (string, error) {
 			now := time.Now()
-			gitHash, err := gomake.ExecGetStdout("git", "rev-parse", "--short", "HEAD")
-			if err != nil {
-				return "", errs.WithE(err, "Failed to get git commit hash")
-			}
 			hms := strings.TrimLeft(now.Format("1504"), "0")
 			if hms == "" {
 				hms = "0"
 			}
-			return fmt.Sprintf("%s.%s.%s-H%s", "0", now.Format("060102"), hms, gitHash), nil
+			return fmt.Sprintf("%s.%s.%s", now.Format("06"), now.Format("0102"), hms), nil
 		}).
 		WithStep(&gomake.StepBuild{
 			PreBuildHook: func(build gomake.StepBuild) error {
