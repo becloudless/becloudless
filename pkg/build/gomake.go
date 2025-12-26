c4175cb46595ec36db1b5c79f2b52e70ada1cc6f
Arnaud Lemaire <alemaire@norad.fr>
2025-12-27 00:30:01 +0100

    fix(ci): rollback release version to add back fixed major version

--- pkg/build/gomake.go
+++ pkg/build/gomake.go
@@ -7,6 +7,7 @@ import (
 	"strings"
 	"time"
 
+	"github.com/n0rad/go-erlog/errs"
 	"github.com/n0rad/gomake"
 )
 
@@ -15,11 +16,15 @@ func main() {
 		WithName("bcl").
 		WithVersionFunc(func() (string, error) {
 			now := time.Now()
+			gitHash, err := gomake.ExecGetStdout("git", "rev-parse", "--short", "HEAD")
+			if err != nil {
+				return "", errs.WithE(err, "Failed to get git commit hash")
+			}
 			hms := strings.TrimLeft(now.Format("1504"), "0")
 			if hms == "" {
 				hms = "0"
 			}
-			return fmt.Sprintf("%s.%s.%s", now.Format("06"), now.Format("0102"), hms), nil
+			return fmt.Sprintf("%s.%s.%s-H%s", "0", now.Format("060102"), hms, gitHash), nil
 		}).
 		WithStep(&gomake.StepBuild{
 			PreBuildHook: func(build gomake.StepBuild) error {
