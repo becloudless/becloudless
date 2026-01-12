687ad475e72980a13374a057e5a7a273f50c6fd0
Arnaud Lemaire <agithub@lmr.fr>
2026-01-12 21:29:52 +0100

    fix(nixos/install): increase wait for wifi time to 10sec (#156)

--- nixos/modules/nixos/roles/install.nix
+++ nixos/modules/nixos/roles/install.nix
@@ -26,7 +26,7 @@
 
 
     # give time to dhcp to get IP, so it will be display
-    services.getty.extraArgs = [ "--delay=5" ];
+    services.getty.extraArgs = [ "--delay=10" ];
     environment.etc."issue.d/ip.issue".text = "\\4\n";
     networking.dhcpcd.runHook = "${pkgs.utillinux}/bin/agetty --reload";
 
