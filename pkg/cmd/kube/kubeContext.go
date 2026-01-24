20c1f416e0bf1ff06335ac4a59613b7848e734c7
Arnaud Lemaire <alemaire@norad.fr>
2026-01-24 02:02:37 +0100

    feat(cli/kube): Add aliases to context

--- pkg/cmd/kube/kubeContext.go
+++ pkg/cmd/kube/kubeContext.go
@@ -8,6 +8,8 @@ import (
 )
 
 func kubeContextCmd() *cobra.Command {
+	var aliases bool
+
 	cmd := cobra.Command{
 		Use:     "context",
 		Aliases: []string{"ctx"},
@@ -17,9 +19,29 @@ func kubeContextCmd() *cobra.Command {
 			if err != nil {
 				return err
 			}
+
 			fmt.Print(context.ToEnv())
+
+			if aliases {
+				if context.Cluster == "" {
+					fmt.Println(`alias k=kubectl`)
+					fmt.Println(`unalias stern 2>/dev/null`)
+					fmt.Println(`unalias k9s 2>/dev/null`)
+				} else if context.Namespace == "" {
+					fmt.Println(`alias k=kubectl`)
+					fmt.Println(`unalias stern 2>/dev/null`)
+					fmt.Println(`unalias k9s 2>/dev/null`)
+				} else {
+					fmt.Printf("alias k='kubectl -n %s'\n", context.Namespace)
+					fmt.Printf("alias stern='stern -n %s'\n", context.Namespace)
+					fmt.Printf("alias k9s='k9s -n %s'\n", context.Namespace)
+				}
+			}
 			return nil
 		},
 	}
+
+	cmd.Flags().BoolVarP(&aliases, "aliases", "a", true, "Alias k, stern and k9s to current context")
+
 	return &cmd
 }
