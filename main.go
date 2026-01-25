99d344ea199665728b2a603192517f620f1afdc7
Arnaud Lemaire <alemaire@norad.fr>
2026-01-26 00:42:54 +0100

    feat(cli): Add signal handler to clean secrets from memory

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"embed"
 	"os"
+	"os/signal"
 
+	"github.com/awnumar/memguard"
 	"github.com/becloudless/becloudless/pkg/bcl"
 	"github.com/becloudless/becloudless/pkg/cmd"
 	"github.com/n0rad/go-erlog/logs"
@@ -19,8 +21,29 @@ func main() {
 	bcl.BCL.Embedded = &Embedded
 	bcl.BCL.Version = Version
 
+	handleSignals()
+
 	if err := cmd.RootCmd().Execute(); err != nil {
 		logs.WithE(err).Fatal("Command failed")
 	}
 	os.Exit(0)
+}
+
+func handleSignals() {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, os.Kill)
+	go func() {
+		for s := range c {
+			logs.WithField("signal", s).Trace("Signal received. Purging Secrets")
+			memguard.Purge()
+			switch s {
+			case os.Interrupt:
+				os.Exit(130)
+			case os.Kill:
+				os.Exit(143)
+			default:
+				os.Exit(42)
+			}
+		}
+	}()
 }
