7ad12a8aefdfad637450f4979e8038e0ed402608
Arnaud Lemaire <alemaire@norad.fr>
2026-02-12 18:22:37 +0100

    up

---
+++ tests/basic/repository/nixos/modules/nixos/groups/test-server/default.nix
@@ -0,0 +1,22 @@
+{config, lib, ...}:
+{
+  config = lib.mkIf (config.bcl.group.name == "test-server") {
+    bcl.role.name = "serverKube";
+    bcl.role.serverKube = {
+      clusterName = "test";
+      clusterNumber = 1;
+      clusterSize = 2;
+    };
+    bcl.role.secretFile = ./default.secrets.yaml;
+
+    bcl.boot.initrdSSHPrivateKey = ''
+      -----BEGIN OPENSSH PRIVATE KEY-----
+      b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAAAMwAAAAtzc2gtZW
+      QyNTUxOQAAACCoZb7aDq3AO3uM6o74iCwqAW49kp0Y/85P/jMxOg4KgAAAAJDfGN0k3xjd
+      JAAAAAtzc2gtZWQyNTUxOQAAACCoZb7aDq3AO3uM6o74iCwqAW49kp0Y/85P/jMxOg4KgA
+      AAAEBMSEXe/kgbeU5BHQW5ZLiTsOurPF1xx84gEcEgrwzvTKhlvtoOrcA7e4zqjviILCoB
+      bj2SnRj/zk/+MzE6DgqAAAAACm4wcmFkQG4wbDIBAgM=
+      -----END OPENSSH PRIVATE KEY-----
+    '';
+  };
+}
