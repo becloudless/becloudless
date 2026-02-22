5679a331fc20f6446c083fdbaaf39117dbcaa88b
Arnaud Lemaire <alemaire@norad.fr>
2026-02-22 18:13:37 +0100

    up

--- nixos/packages/bcl/default.nix
+++ nixos/packages/bcl/default.nix
@@ -1,6 +1,7 @@
 { lib, stdenv, fetchurl }:
 
 let
+  # renovate: datasource=github-releases depName=becloudless/becloudless extractVersion=^cli-v(?<version>.+)$
   version = "0.260222.1646-H393a363";
 
   # Map Nix system to Go platform (GOOS-GOARCH format)
@@ -11,8 +12,10 @@ let
     "aarch64-darwin" = "darwin-arm64";
   }.${stdenv.hostPlatform.system} or (throw "bcl: unsupported system ${stdenv.hostPlatform.system}");
 
+  # To recompute hashes after a version bump, run for each platform (linux-amd64, linux-arm64, darwin-amd64, darwin-arm64):
+  #   nix store prefetch-file --hash-type sha256 --json "https://github.com/becloudless/becloudless/releases/download/cli-v<version>/bcl-<platform>.tar.gz" | jq -r .hash
   hashes = {
-    "linux-amd64"  = "sha256-3bNbRxxqqF6cWnTZoeHyKZqCk8U/C7otZscQAVrTA8o=";
+    "linux-amd64"  = "sha256-3bNbRxxqql6cWnTZoeHyKZqCk8U/C7otZscQAVrTA8o=";
     "linux-arm64"  = "sha256-hpVK8QVzu/CafdMi7y8aSR7rJNApyruo/b3xCu7g+fY=";
     "darwin-amd64" = "sha256-jL1z9qcYtVtpW4V0MQpjxjqOzodXLmN8Jb1a2VH8hJ0=";
     "darwin-arm64" = "sha256-pxZGDJuDtr9OJu1w1D63LMJgEhiYaDtl3JB3QfMpy4k=";
@@ -32,7 +35,7 @@ stdenv.mkDerivation {
 
   installPhase = ''
     mkdir -p $out/bin
-    cp bcl $out/bin/bcl
+    cp bcl-${platform}/bcl $out/bin/bcl
     chmod +x $out/bin/bcl
   '';
 
