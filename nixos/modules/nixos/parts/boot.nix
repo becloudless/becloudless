13bcd0f47e76deedd29e9a49f98c3fe03cba9fae
Arnaud Lemaire <agithub@lmr.fr>
2026-02-25 00:27:56 +0100

    fix(nixos): uboot does not support secrets in initrd (#330)

--- nixos/modules/nixos/parts/boot.nix
+++ nixos/modules/nixos/parts/boot.nix
@@ -90,7 +90,8 @@ in {
           enable = true;
           port = 22;
           authorizedKeys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILvM8t4hXJxjBzrUS5FhAQ/TD9TJscT7CyLKFSOjZjj4 id_ed25519" ];
-          hostKeys = [ "/etc/ssh/initrd_ssh_host_ed25519_key" ];
+          # uboot does not support secrets in initrd, so we give a path to have the key in the store
+          hostKeys = if cfg.loader == "uboot" then [ /etc/ssh/initrd_ssh_host_ed25519_key ] else [ "/etc/ssh/initrd_ssh_host_ed25519_key" ];
         };
         # postDeviceCommands = lib.mkAfter ''
         #   zfs rollback -r rpool/local/root@blank
