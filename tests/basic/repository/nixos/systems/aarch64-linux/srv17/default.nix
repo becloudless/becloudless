ab07de5466c17e410ad24525a4626b0313d0d746
Arnaud Lemaire <alemaire@norad.fr>
2026-02-26 18:12:31 +0100

    up

---
+++ tests/basic/repository/nixos/systems/aarch64-linux/srv17/default.nix
@@ -0,0 +1,13 @@
+{lib, ...}:
+{
+
+  bcl.hardware.device = "orangepi5plus";
+  bcl.disk.encrypted = lib.mkForce false;
+  bcl.system = {
+    enable = true;
+    group = "server";
+    ids = "networkMacs=c0:74:2b:ff:5d:96,c0:74:2b:ff:5d:97";
+    devices = [ "/dev/disk/by-id/nvme-CT4000P3SSD8_2312E6BF61B3" ];
+  };
+
+}
