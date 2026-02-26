f7747a0b8990979466a8e3dd2f29857004ebf353
Arnaud Lemaire <alemaire@norad.fr>
2026-02-26 09:03:57 +0100

    up

--- nixos/shells/kernel/default.nix
+++ nixos/shells/kernel/default.nix
@@ -13,18 +13,25 @@
 # git remote add origin https://github.com/Joshua-Riek/linux-rockchip.git
 # git fetch --depth 1 origin e21cf49ee9a41a02846da050a6930e317bc99b68
 # git checkout FETCH_HEAD
-
+# cp /nix/bcl/becloudless/nixos/packages/orangepi-kernel/rk35xx_vendor_config .config
+#
 
 mkShell {
     packages = with pkgs; [
-      qemu_kvm
-      qemu-utils
-    ];
-
-    overlays = [
-      pkgs.nixos-anywhere
+      pkg-config
+      ncurses
+      gcc
+      flex
+      bison
+      bc
+      openssl
     ];
 
     shellHook = ''
+        export DEBUG=1
       '';
 }
+
+# build phase by phase
+# https://discourse.nixos.org/t/nix-build-phases-run-nix-build-phases-interactively/36090
+# https://github.com/imincik/nix-utils/blob/5278ab4635672d0537f032d785f4e0bf1842f2e3/nix-build-phases.bash
