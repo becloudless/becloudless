179633b6cfc83aabef4d98223295c61a28bf4794
Arnaud Lemaire <alemaire@norad.fr>
2026-02-26 18:15:58 +0100

    up

--- tests/basic/repository/nixos/systems/aarch64-linux/srv17/default.nix
+++ tests/basic/repository/nixos/systems/aarch64-linux/srv17/default.nix
@@ -5,7 +5,7 @@
   bcl.disk.encrypted = lib.mkForce false;
   bcl.system = {
     enable = true;
-    group = "server";
+    group = "test-server";
     ids = "networkMacs=c0:74:2b:ff:5d:96,c0:74:2b:ff:5d:97";
     devices = [ "/dev/disk/by-id/nvme-CT4000P3SSD8_2312E6BF61B3" ];
   };
