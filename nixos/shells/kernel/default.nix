9271e25fb9d2449bf721def275cd021aa8c757cd
Arnaud Lemaire <alemaire@norad.fr>
2026-02-26 09:00:36 +0100

    up

--- nixos/shells/kernel/default.nix
+++ nixos/shells/kernel/default.nix
@@ -7,6 +7,14 @@
     ...
 }:
 
+# mkdir kernel
+# cd kernel
+# git init
+# git remote add origin https://github.com/Joshua-Riek/linux-rockchip.git
+# git fetch --depth 1 origin e21cf49ee9a41a02846da050a6930e317bc99b68
+# git checkout FETCH_HEAD
+
+
 mkShell {
     packages = with pkgs; [
       qemu_kvm
