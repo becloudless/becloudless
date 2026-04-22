c08f7650ab479cfcbbf26e2abf6f66ba18eff19e
Arnaud Lemaire <alemaire@norad.fr>
2026-04-22 17:02:50 +0200

    feat(nixos): Add RedistributableFirmware by default

--- nixos/modules/nixos/hardware/default.nix
+++ nixos/modules/nixos/hardware/default.nix
@@ -21,25 +21,35 @@
       default = [];
       description = "List of valid commons identifiers. Each common module registers itself here.";
     };
+    enableRedistributableFirmware = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = "Whether to enable redistributable firmware.";
+    };
   };
 
-  config = lib.mkIf (config.bcl.hardware.device != "") {
-    assertions = [
-      {
-        assertion = builtins.elem config.bcl.hardware.device config.bcl.hardware.knownDevices;
-        message = ''
-          bcl.hardware.device is set to "${config.bcl.hardware.device}" which is not a known device.
-          Known devices: ${lib.concatStringsSep ", " config.bcl.hardware.knownDevices}
-          Make sure the corresponding device module is imported.
-        '';
-      }
-    ] ++ map (c: {
-      assertion = builtins.elem c config.bcl.hardware.knownCommons;
-      message = ''
-        bcl.hardware.commons contains "${c}" which is not a known common.
-        Known commons: ${lib.concatStringsSep ", " config.bcl.hardware.knownCommons}
-        Make sure the corresponding common module is imported.
-      '';
-    }) config.bcl.hardware.commons;
-  };
+  config = lib.mkMerge [
+    {
+      hardware.enableRedistributableFirmware = config.bcl.hardware.enableRedistributableFirmware;
+    }
+    (lib.mkIf (config.bcl.hardware.device != "") {
+         assertions = [
+           {
+             assertion = builtins.elem config.bcl.hardware.device config.bcl.hardware.knownDevices;
+             message = ''
+               bcl.hardware.device is set to "${config.bcl.hardware.device}" which is not a known device.
+               Known devices: ${lib.concatStringsSep ", " config.bcl.hardware.knownDevices}
+               Make sure the corresponding device module is imported.
+             '';
+           }
+         ] ++ map (c: {
+           assertion = builtins.elem c config.bcl.hardware.knownCommons;
+           message = ''
+             bcl.hardware.commons contains "${c}" which is not a known common.
+             Known commons: ${lib.concatStringsSep ", " config.bcl.hardware.knownCommons}
+             Make sure the corresponding common module is imported.
+           '';
+         }) config.bcl.hardware.commons;
+       })
+  ];
 }
