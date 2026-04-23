4e5c9805413437b416252b7140769c246bf7dcbd
Arnaud Lemaire <alemaire@norad.fr>
2026-04-23 15:34:41 +0200

    feat(nixos): persist cosmic config

--- nixos/modules/nixos/parts/wm/cosmic.nix
+++ nixos/modules/nixos/parts/wm/cosmic.nix
@@ -8,10 +8,22 @@ in
     services.displayManager.cosmic-greeter.enable = true;
     services.desktopManager.cosmic.enable = true;
     services.system76-scheduler.enable = true;
+#    services.xserver.xkb.layout = "fr";
 
     programs.firefox.preferences = {
         # disable libadwaita theming for Firefox
         "widget.gtk.libadwaita-colors.enabled" = false;
       };
+
+    environment.persistence = lib.mkMerge (
+      lib.mapAttrsToList (name: ucfg:
+        lib.optionalAttrs (ucfg.syncthing.homeFolderId != "") {
+          "/nix/syncthing/homes" = {
+            hideMounts = true;
+            users."${name}".directories = [ ".config/cosmic" ];
+          };
+        }
+      ) cosmicUsers
+    );
   };
 }
