2e93f43024792d52c6412a345bc0793d005560b3
Arnaud Lemaire <alemaire@norad.fr>
2026-04-25 18:54:11 +0200

    up

--- nixos/modules/nixos/parts/users.nix
+++ nixos/modules/nixos/parts/users.nix
@@ -2,8 +2,6 @@
 
 let
   cfg = config.bcl.users;
-  stUsers = lib.filterAttrs (_: u: u.syncthing.enable) cfg;
-  nixosConfig = config;
 
   userOpts = { name, config, ... }: {
     options = {
@@ -79,13 +77,6 @@ in
         user = autoLoginUser;
       };
 
-      assertions = lib.concatLists (lib.mapAttrsToList (name: ucfg: [
-        {
-          assertion = !ucfg.syncthing.enable || ucfg.syncthing.sopsFile != null;
-          message = "bcl.users.${name}.syncthing.sopsFile must be set when syncthing is enabled.";
-        }
-      ]) cfg);
-
       users.users = lib.mapAttrs (name: ucfg:
         {
           isNormalUser = true;
@@ -96,31 +87,14 @@ in
         }
       ) cfg;
 
-      sops.secrets = lib.mkMerge (
-        # user passwords
-        (lib.mapAttrsToList (name: ucfg:
-          lib.optionalAttrs (ucfg.sopsFile != null) {
-            "users.${name}.hashedPassword" = {
-              neededForUsers = true;
-              sopsFile = ucfg.sopsFile;
-            };
-          }
-        ) cfg)
-        ++
-        # syncthing certs
-        (lib.mapAttrsToList (name: ucfg: {
-          "users.${name}.syncthing.cert" = {
-            owner = name;
-            sopsFile = ucfg.syncthing.sopsFile;
-            path = "/nix/home/${name}/.local/state/syncthing/cert.pem";
+      sops.secrets = lib.mkMerge (lib.mapAttrsToList (name: ucfg:
+        lib.optionalAttrs (ucfg.sopsFile != null) {
+          "users.${name}.hashedPassword" = {
+            neededForUsers = true;
+            sopsFile = ucfg.sopsFile;
           };
-          "users.${name}.syncthing.key" = {
-            owner = name;
-            sopsFile = ucfg.syncthing.sopsFile;
-            path = "/nix/home/${name}/.local/state/syncthing/key.pem";
-          };
-        }) stUsers)
-      );
+        }
+      ) cfg);
 
       home-manager.users = lib.mapAttrs (name: ucfg: { lib, pkgs, config, ... }: {
         imports = [ (inputs.impermanence + "/home-manager.nix") ];
@@ -170,49 +144,7 @@ in
         '';
 
         home.stateVersion = "23.11"; # never touch that
-
-        services.syncthing = lib.mkIf ucfg.syncthing.enable {
-          enable = true;
-          settings = {
-            options = {
-              localAnnounceEnabled = false;
-              relaysEnabled = true;
-              urAccepted = -1;
-              listenAddresses = [ "relay://syncthing.${nixosConfig.bcl.global.domain}:22067/?id=${nixosConfig.bcl.global.syncthing.relayId}" ];
-            };
-            devices."${name}.syncthing.${nixosConfig.bcl.global.domain}" = {
-              id = ucfg.syncthing.remote.id;
-              autoAcceptFolders = false;
-            };
-            folders =
-              (lib.mapAttrs' (k: v: lib.nameValuePair k {
-                id = v.id;
-                path = "/nix/syncthing/home/${name}/${k}";
-                devices = [ "${name}.syncthing.${nixosConfig.bcl.global.domain}" ];
-              }) ucfg.syncthing.folders)
-              // lib.optionalAttrs (ucfg.syncthing.homeFolderId != "") {
-                "Home" = {
-                  id = ucfg.syncthing.homeFolderId;
-                  path = "/nix/syncthing/homes/home/${name}";
-                  devices = [ "${name}.syncthing.${nixosConfig.bcl.global.domain}" ];
-                  fsWatcherEnabled = false;
-                  rescanIntervalS = 3600;
-                };
-              };
-          };
-        };
       }) cfg;
-
-      # TODO this is gnome specific
-      system.activationScripts = lib.mkMerge (lib.mapAttrsToList (name: ucfg: {
-        "accountsservice-icon-${name}" = {
-          text = ''
-            mkdir -p /var/lib/AccountsService/{icons,users}
-            echo -e "[User]\nSession=gnome\nIcon=/var/lib/AccountsService/icons/${name}\n" > /var/lib/AccountsService/users/${name}
-            cp /nix/syncthing/home/${name}/Pictures/face.png /var/lib/AccountsService/icons/${name} || true
-          '';
-        };
-      }) stUsers);
 
       systemd.tmpfiles.rules = lib.concatLists (lib.mapAttrsToList (name: ucfg: [
         "d /nix/home/${name}/.local 0700 ${name} users"
@@ -225,74 +157,21 @@ in
         "d /nix/home/${name}/Tmp 0700 ${name} users"
       ]) cfg);
 
-      environment.persistence = lib.mkMerge (
-        [
-          {
-            # keep on the system disk
-            "/nix" = {
-              hideMounts = true;
-              users = lib.mapAttrs (_: _: {
-                directories = [
-                  ".cache"
-                  "Tmp"
-                  ".local/share/docker" # for rootless docker
-                  ".local/state/wireplumber" # audio setup
-                  ".local/state/syncthing" # systemd user unit state
-                  ".local/share/com.unicornsonlsd.finamp"
-                  ".config/VirtualBox"
-                  ".mozilla"  # # firefox/ and native-messaging-hosts/ for keepassxc
-                ];
-              }) cfg;
-            };
-          }
-        ]
-        ++
-        lib.mapAttrsToList (name: ucfg:
-          {
-            # keep on syncthing folders
-            "/nix/syncthing" = {
-              hideMounts = true;
-              users."${name}".directories = lib.attrNames ucfg.syncthing.folders;
-            };
-          } // lib.optionalAttrs (ucfg.syncthing.homeFolderId != "") {
-            # keep from home folder on syncthing 'Home' folder
-            "/nix/syncthing/homes" = {
-              hideMounts = true;
-              users."${name}" = {
-                directories = [
-                  
-                  # ".viminfo"
-                  ".tmux"
-                  # ".lesshst" # less replace the file
-                  ".docker" # must be the directory so it can mounted by bbc commands
-                  ".vscode-oss"
-                  ".config/chromium"
-                  ".config/Signal"
-                  ".local/bin" # scripts and bbc
-                  ".local/share/applications" # to keep plex chromium application
-                  ".local/share/desktop-directories"
-                  ".local/share/zoxide"
-                  ".config/menus" # .desktop applications into menu
-                  ".local/share/icons" # .desktop icons
-                  ".config/gcloud"
-                  ".config/sops" # TODO replace by static
-                  ".config/VSCodium"
-                  ".config/keepassxc"
-                  ".wine"
-
-                  ".local/share/JetBrains/" # plugins and license
-                  ".config/JetBrains" # looks required for license
-                  ".java/.userPrefs/jetbrains"
-                ];
-                files = [
-                  ".z"
-                  ".local/share/gnome-shell/application_state" # trusted .desktop applications
-                ];
-              };
-            };
-          }
-        ) stUsers
-      );
+      environment.persistence."/nix" = {
+        hideMounts = true;
+        users = lib.mapAttrs (_: _: {
+          directories = [
+            ".cache"
+            "Tmp"
+            ".local/share/docker" # for rootless docker
+            ".local/state/wireplumber" # audio setup
+            ".local/state/syncthing" # systemd user unit state
+            ".local/share/com.unicornsonlsd.finamp"
+            ".config/VirtualBox"
+            ".mozilla"  # firefox/ and native-messaging-hosts/ for keepassxc
+          ];
+        }) cfg;
+      };
 
     };
 }
