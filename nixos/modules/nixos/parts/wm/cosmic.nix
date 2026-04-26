5ba47a8c1ea4493f29a70c2c8426e3d86c193c4c
Arnaud Lemaire <alemaire@norad.fr>
2026-04-26 15:25:04 +0200

    up

--- nixos/modules/nixos/parts/wm/cosmic.nix
+++ nixos/modules/nixos/parts/wm/cosmic.nix
@@ -2,6 +2,7 @@
 
 let
   cosmicUsers = lib.filterAttrs (name: ucfg: ucfg.wm == "cosmic") config.bcl.users.users;
+  unstable = inputs.nixpkgs-unstable.legacyPackages.${pkgs.system};
 in
 {
   config = lib.mkIf (cosmicUsers != {}) {
@@ -9,6 +10,69 @@ in
     services.desktopManager.cosmic.enable = true;
     services.system76-scheduler.enable = true;
     services.flatpak.enable = true; # for cosmic-store
+
+
+    # unstable
+    services.displayManager.cosmic-greeter.package = with unstable; cosmic-greeter;
+    environment.cosmic.excludePackages = with pkgs; [
+      cosmic-applets
+      cosmic-applibrary
+      cosmic-bg
+      cosmic-comp
+      cosmic-files
+      cosmic-idle
+      cosmic-initial-setup
+      cosmic-launcher
+      cosmic-notifications
+      cosmic-osd
+      cosmic-panel
+      cosmic-session
+      cosmic-settings
+      cosmic-settings-daemon
+      cosmic-workspaces-epoch
+      # not core
+      cosmic-edit
+      cosmic-icons
+      cosmic-player
+      cosmic-randr
+      cosmic-screenshot
+      cosmic-term
+      cosmic-wallpapers
+      pop-icon-theme
+      pop-launcher
+      cosmic-store
+    ];
+    environment.systemPackages = with unstable; [
+      cosmic-applets
+      cosmic-applibrary
+      cosmic-bg
+      cosmic-comp
+      cosmic-files
+      cosmic-idle
+      cosmic-initial-setup
+      cosmic-launcher
+      cosmic-notifications
+      cosmic-osd
+      cosmic-panel
+      cosmic-session
+      cosmic-settings
+      cosmic-settings-daemon
+      cosmic-workspaces-epoch
+      # not core
+      cosmic-edit
+      cosmic-icons
+      cosmic-player
+      cosmic-randr
+      cosmic-screenshot
+      cosmic-term
+      cosmic-wallpapers
+      pop-icon-theme
+      pop-launcher
+      cosmic-store
+    ];
+
+
+
 
     programs.firefox.preferences = {
         # disable libadwaita theming for Firefox
