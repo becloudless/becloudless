9dff506029bacdeaddaa2b6f8980ab2d4e07abae
Arnaud Lemaire <alemaire@norad.fr>
2026-05-07 00:06:29 +0200

    up

---
+++ nixos/modules/nixos/parts/disks.nix
@@ -0,0 +1,117 @@
+{ config, lib, ... }:
+let
+  cfg = config.bcl.disks;
+
+  modeToLevel = {
+    raid0  =  0;
+    raid1  =  1;
+    raid5  =  5;
+    raid6  =  6;
+    raid10 = 10;
+  };
+
+  isRaid = diskCfg: builtins.length diskCfg.devices > 1;
+
+  # Filesystem (or LUKS-wrapped filesystem) content leaf
+  fsContent = name: diskCfg:
+    if diskCfg.encrypted then {
+      type    = "luks";
+      name    = name;
+      settings.allowDiscards = true;
+      passwordFile = "/root/secret.key";
+      content = {
+        type       = "filesystem";
+        format     = diskCfg.format;
+        mountpoint = diskCfg.path;
+      };
+    } else {
+      type       = "filesystem";
+      format     = diskCfg.format;
+      mountpoint = diskCfg.path;
+    };
+
+  # One disko disk entry per physical device
+  mkDiskEntries = name: diskCfg:
+    builtins.listToAttrs (lib.imap0 (i: device: {
+      name  = "${name}_${toString i}";
+      value = {
+        type    = "disk";
+        device  = device;
+        content = {
+          type       = "gpt";
+          partitions.primary = {
+            size    = "100%";
+            content = if isRaid diskCfg then {
+              type = "mdraid";
+              name = name;
+            } else fsContent name diskCfg;
+          };
+        };
+      };
+    }) diskCfg.devices);
+
+  # One disko mdadm entry per RAID disk
+  mkMdadmEntry = name: diskCfg: {
+    type    = "mdadm";
+    level   = modeToLevel.${diskCfg.mode};
+    content = fsContent name diskCfg;
+  };
+
+  allDiskEntries =
+    builtins.foldl' (acc: e: acc // e) {}
+      (lib.mapAttrsToList mkDiskEntries cfg);
+
+  raidConfigs  = lib.filterAttrs (_: diskCfg: isRaid diskCfg) cfg;
+  mdadmEntries = lib.mapAttrs mkMdadmEntry raidConfigs;
+
+in {
+  options.bcl.disks = lib.mkOption {
+    default     = {};
+    description = "Named data-disk configurations managed by disko.";
+    example     = {
+      ssd1 = {
+        path      = "/disks/ssd1";
+        encrypted = true;
+        format    = "btrfs";
+        mode      = "raid0";       # default, used when devices > 1
+        devices   = [ "/dev/disk/by-id/xxx" "/dev/disk/by-id/yyy" ];
+      };
+    };
+    type = lib.types.attrsOf (lib.types.submodule ({ ... }: {
+      options = {
+        path = lib.mkOption {
+          type        = lib.types.str;
+          description = "Mount point for the assembled volume (e.g. /disks/ssd1).";
+        };
+        encrypted = lib.mkOption {
+          type        = lib.types.bool;
+          default     = false;
+          description = "Wrap the filesystem in a LUKS container.";
+        };
+        format = lib.mkOption {
+          type        = lib.types.str;
+          default     = "btrfs";
+          description = "Filesystem type passed to mkfs (e.g. ext4, btrfs, xfs).";
+        };
+        mode = lib.mkOption {
+          type        = lib.types.enum [ "raid0" "raid1" "raid5" "raid6" "raid10" ];
+          default     = "raid0";
+          description = ''
+            mdadm RAID level to use when multiple devices are provided.
+            Ignored when only a single device is specified.
+          '';
+        };
+        devices = lib.mkOption {
+          type        = lib.types.listOf lib.types.str;
+          description = "Ordered list of block-device paths that make up this volume.";
+        };
+      };
+    }));
+  };
+
+  config = lib.mkIf (cfg != {}) {
+    disko.devices =
+      { disk = allDiskEntries; }
+      // lib.optionalAttrs (raidConfigs != {}) { mdadm = mdadmEntries; };
+  };
+}
--- nixos/tests/basic/repository/nixos/systems/x86_64-linux/test-srv12/default.nix
+++ nixos/tests/basic/repository/nixos/systems/x86_64-linux/test-srv12/default.nix
@@ -7,5 +7,16 @@
       ids = "motherboardUuid=22222222-2222-2222-2222-222222222222";
       devices = [ "/dev/sda" ];
     };
+
+    disks = {
+      ssd1 = {
+        path=/disks/ssd1;
+        encrypted = true;
+        format = "btrfs";
+        mode = "raid0";
+        devices = ["/dev/disks/by-id/xxx" "/dev/disks/by-id/yyy"];
+      };
+    };
+
   };
 }
