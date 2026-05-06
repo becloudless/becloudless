c84166b1c97fde1e7cb11a11839eb46374039354
Arnaud Lemaire <agithub@lmr.fr>
2026-05-06 21:45:14 +0200

    feat(nixos): Add usbguard to all devices (#541)

---
+++ nixos/modules/nixos/role/global/usb.nix
@@ -0,0 +1,25 @@
+{ config, lib, pkgs, ...}:
+{
+  services.usbguard = {
+    enable = true;
+  };
+
+  # All declared users can handle USB access
+  services.usbguard.IPCAllowedUsers = lib.attrNames config.bcl.users.users;
+
+  # Start usbguard-notifier for every bcl user when they open a graphical session
+  home-manager.users = lib.mapAttrs (_: _: { ... }: {
+    systemd.user.services.usbguard-notifier = {
+      Unit = {
+        Description = "USBGuard notification daemon";
+        PartOf = [ "graphical-session.target" ];
+        After = [ "graphical-session.target" ];
+      };
+      Service = {
+        ExecStart = "${pkgs.usbguard-notifier}/bin/usbguard-notifier";
+        Restart = "on-failure";
+      };
+      Install.WantedBy = [ "graphical-session.target" ];
+    };
+  }) config.bcl.users.users;
+}
