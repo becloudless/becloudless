9180b378e0cda639a9fa1bcbe3eef1be3b139f99
Arnaud Lemaire <alemaire@norad.fr>
2026-05-17 00:29:17 +0200

    up

--- cli/main.go
+++ cli/main.go
@@ -16,7 +16,7 @@ import (
 var Embedded embed.FS
 var Version = "0.0.0"
 
-//go:generate ./dist-tools/go-jsonschema -p schema --schema-root-type global=Global ../nixos/modules/nixos/global/default.schema.json -o pkg/generated/schema/schema.go
+//go:generate ./dist-tools/go-jsonschema -p schema --schema-root-type global=Global ../nixos/modules/nixos/bcl/global.schema.json -o pkg/generated/schema/schema.go
 func main() {
 	bcl.BCL.Embedded = &Embedded
 	bcl.BCL.Version = Version
