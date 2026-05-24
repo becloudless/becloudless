87f565c9433c67fc8a1523a06867f554c15bfea7
Arnaud Lemaire <alemaire@norad.fr>
2026-05-24 11:50:14 +0200

    up

--- nixos/packages/bcl/default.nix
+++ nixos/packages/bcl/default.nix
@@ -2,7 +2,7 @@
 
 let
   # renovate: datasource=github-releases depName=becloudless/becloudless
-  version = "0.260524.903";
+  version = "0.260524.933";
 
   # Map Nix system to Go platform (GOOS-GOARCH format)
   platform = {
