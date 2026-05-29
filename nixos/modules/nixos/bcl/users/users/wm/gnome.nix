09fa472d142232fbee2439f1a6542a5ce5a227ef
Arnaud Lemaire <alemaire@norad.fr>
2026-05-29 23:35:17 +0200

    up

--- nixos/modules/nixos/bcl/users/users/wm/gnome.nix
+++ nixos/modules/nixos/bcl/users/users/wm/gnome.nix
@@ -185,20 +185,6 @@ in
         "org/gnome/GWeather4" = {
           temperature-unit = "centigrade";
         };
-    # hardware.printers = {
-    #   ensurePrinters = [
-    #     {
-    #       name = "ip2700";
-    #       location = "Home";
-    #       deviceUri = "usb://Canon/iP2700%20series?serial=F8241F";
-    #       model = "gutenprint.${lib.versions.majorMinor (lib.getVersion pkgs.gutenprint)}://brother-hl-5140/expert";
-    #       ppdOptions = {
-    #         PageSize = "A4";
-    #       };
-    #     }
-    #   ];
-    #   ensureDefaultPrinter = "ip2700";
-    # };
 
         "org/gnome/desktop/peripherals/touchpad" = {
           speed = 0.62790697674418605;
