9e139b5937cfac6bfd7ca1e591aa3af1a312373c
Arnaud Lemaire <alemaire@norad.fr>
2026-05-29 09:51:32 +0200

    up

--- nixos/modules/nixos/bcl/default.nix
+++ nixos/modules/nixos/bcl/default.nix
@@ -1,6 +1,6 @@
 { lib, ... }:
 {
   imports = lib.filter
-              (n: !(lib.strings.hasSuffix "/default.nix" (toString n)) && lib.strings.hasSuffix ".nix" (toString n))
+              (n: n != ./default.nix && lib.strings.hasSuffix ".nix" (toString n))
               (lib.filesystem.listFilesRecursive ../.);
 }
