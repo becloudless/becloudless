c177dd0e2c857625d67340f7bdd74045def1d674
Arnaud Lemaire <alemaire@norad.fr>
2026-05-29 13:44:20 +0200

    up

--- nixos/modules/nixos/bcl/users/users/wm/gnome.nix
+++ nixos/modules/nixos/bcl/users/users/wm/gnome.nix
@@ -88,7 +88,7 @@ in
         gnomeExtensions.workspace-indicator
         gnomeExtensions.wallpaper-slideshow
         gnomeExtensions.quake-terminal
-        bcl.live-lock-screen
+        pkgs.bcl.live-lock-screen
         # gnomeExtensions.smart-auto-move
         
       ];
