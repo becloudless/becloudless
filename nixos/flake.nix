87e496356e4fe30b3fee1a1db3bcb955eeb7e01e
Arnaud Lemaire <alemaire@norad.fr>
2026-05-30 02:58:36 +0200

    up

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -104,7 +104,7 @@
 
   inputs = {
     nixpkgs = {
-      url = "github:NixOS/nixpkgs/nixos-25.11";
+      url = "github:NixOS/nixpkgs/nixos-26.05";
     };
 
     nixpkgs-unstable = {
@@ -126,7 +126,7 @@
     };
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
