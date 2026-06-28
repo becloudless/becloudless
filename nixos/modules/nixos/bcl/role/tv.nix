64fd484f945f063c183b58eb4f76032c4df0512c
Arnaud Lemaire <alemaire@norad.fr>
2026-06-28 10:48:23 +0200

    up

--- nixos/modules/nixos/bcl/role/tv.nix
+++ nixos/modules/nixos/bcl/role/tv.nix
@@ -53,13 +53,14 @@
                 <decoration>none</decoration>
               </core>
               <windowRules>
-                <windowRule title="*" matchOnce="true">
+                <windowRule title="*">
                   <action name="Fullscreen"/>
                 </windowRule>
               </windowRules>
             </labwc_config>
           '';
           jellyfinScript = pkgs.writeShellScript "start-jellyfin" ''
+            echo "=== jellyfin startup ===" >> /tmp/tv-startup.log
             rm -f ~/.cache/jellyfin-desktop/SingletonLock ~/.cache/jellyfin-desktop/SingletonCookie
             randr_out=$(${pkgs.wlr-randr}/bin/wlr-randr 2>/dev/null) || true
             output=$(echo "$randr_out" | grep -m1 '^[A-Za-z]' | awk '{print $1}')
@@ -74,7 +75,10 @@
             mkdir -p ~/.config/jellyfin-desktop ~/.config/labwc
             cp ${jellyfinSettings} ~/.config/jellyfin-desktop/settings.json
             cp ${labwcRc} ~/.config/labwc/rc.xml
-            exec ${pkgs.labwc}/bin/labwc -s ${jellyfinScript}
+            echo "=== rc.xml ===" > /tmp/tv-startup.log
+            cat ~/.config/labwc/rc.xml >> /tmp/tv-startup.log
+            echo "=== labwc start ===" >> /tmp/tv-startup.log
+            exec ${pkgs.labwc}/bin/labwc -d -s ${jellyfinScript} >> /tmp/tv-startup.log 2>&1
           '';
         in "${startScript}";
         user = "tv";
