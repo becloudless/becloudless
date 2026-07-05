4050ef6d4e69597961b2bfe00571dc712ca9251d
Arnaud Lemaire <alemaire@norad.fr>
2026-07-05 10:52:26 +0200

    up

--- nixos/modules/nixos/bcl/role/tvScreensaver.nix
+++ nixos/modules/nixos/bcl/role/tvScreensaver.nix
@@ -38,7 +38,7 @@ in
             echo "No playlist available at $PLAYLIST, waiting for sync..."
             return
           fi
-          mpv --fs --loop-playlist=inf --shuffle --image-display-duration=30 --no-osd-bar --panscan=0 --scale=bilinear --video-unscaled=no --mute=yes --speed=0.5 --osd-playing-msg=\''${media-title} --osd-duration=3600000 --osd-font-size=12 "$PLAYLIST" &
+          mpv --fs --loop-playlist=inf --shuffle --image-display-duration=30 --no-osd-bar --panscan=0 --scale=bilinear --video-unscaled=no --mute=yes --speed=0.5 --osd-playing-msg=\''${media-title} --osd-duration=3600000 --osd-font-size=12 --osd-align-x=left --osd-align-y=bottom "$PLAYLIST" &
         }
 
         ############################
