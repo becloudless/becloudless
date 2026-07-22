5c1ded0ac9d5148774819b0576e0a78b936516e8
Arnaud Lemaire <alemaire@norad.fr>
2026-07-22 23:59:17 +0200

    up

--- nixos/modules/nixos/bcl/default.nix
+++ nixos/modules/nixos/bcl/default.nix
@@ -1,6 +1,10 @@
 { lib, ... }:
 {
+  # Nested `default.nix` files (e.g. role/default.nix) are already wired in as
+  # their own namespaced modules by snowfall-lib's folder convention, so they
+  # must be excluded here to avoid importing (and thus declaring their
+  # options) twice.
   imports = lib.filter
-              (n: n != ./default.nix && lib.strings.hasSuffix ".nix" (toString n))
+              (n: lib.strings.hasSuffix ".nix" (toString n) && baseNameOf (toString n) != "default.nix")
               (lib.filesystem.listFilesRecursive ../.);
 }
