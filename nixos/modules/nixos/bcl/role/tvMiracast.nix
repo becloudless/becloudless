1cc4c8f1505b01ada079a8983de5bc80d38be3ee
Arnaud Lemaire <alemaire@norad.fr>
2026-08-02 12:00:34 +0200

    miracast enabled by default

--- nixos/modules/nixos/bcl/role/tvMiracast.nix
+++ nixos/modules/nixos/bcl/role/tvMiracast.nix
@@ -191,7 +191,7 @@ in
   options.bcl.role.tv.miracast = {
     enable = lib.mkOption {
       type = lib.types.bool;
-      default = false;
+      default = true;
       description = ''
         Whether to enable the Miracast (Wi-Fi Display) receiver, to cast
         from Android devices without Google/Chromecast.
