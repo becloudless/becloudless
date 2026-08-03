72e14e748a13e34d3e5293a0f5fe56b535b06c13
Arnaud Lemaire <alemaire@norad.fr>
2026-08-03 19:13:14 +0200

    up

--- cli/pkg/cmd/nixos/nixosUpgrade.go
+++ cli/pkg/cmd/nixos/nixosUpgrade.go
@@ -3,13 +3,10 @@ package nixos
 import (
 	"fmt"
 	"os"
-	"path/filepath"
 	"strings"
 
 	"github.com/becloudless/becloudless/pkg/bcl"
-	bclGit "github.com/becloudless/becloudless/pkg/git"
 	"github.com/becloudless/becloudless/pkg/system/runner"
-	"github.com/go-git/go-git/v6"
 	"github.com/n0rad/go-erlog/errs"
 	"github.com/n0rad/go-erlog/logs"
 	"github.com/spf13/cobra"
@@ -56,6 +53,7 @@ func nixosUpgradeCmd() *cobra.Command {
 	var action string
 	var sshConfig runner.SshConnectionConfig
 	var systemName string
+	var useLocalGitRepository bool
 
 	cmd := &cobra.Command{
 		Use:     "upgrade",
@@ -64,48 +62,57 @@ func nixosUpgradeCmd() *cobra.Command {
 		Long:    "Small wrapper around nixos-rebuild to upgrade NixOS system from current infra git repo",
 		RunE: func(cmd *cobra.Command, args []string) error {
 			if sshConfig.Host == "" {
-				if os.Geteuid() != 0 {
-					return errs.With("Updating current system requires root privileges, please run with sudo")
-				}
+				if useLocalGitRepository {
+					// repository, err := bclGit.OpenRepository(".")
+					// if errs.Is(err, git.ErrRepositoryNotExists) {
+					// 	logs.WithField("repo", bcl.BCL.System.Repository).Info("Not in an infra folder, updating using upstream git repository")
 
-				repository, err := bclGit.OpenRepository(".")
-				if errs.Is(err, git.ErrRepositoryNotExists) {
-					logs.WithField("repo", bcl.BCL.System.Repository).Info("Not in an infra folder, updating using upstream git repository")
+					// 	if bcl.BCL.System.Repository == "" {
+					// 		return errs.With("No repository configured in bcl config")
+					// 	}
 
-					if bcl.BCL.System.Repository == "" {
-						return errs.With("No repository configured in bcl config")
-					}
+					// } else if err != nil {
+					// 	return errs.WithE(err, "failed to open git repository")
+					// } else {
+					// 	// Nix build process only files that are in git state
+					// 	if err := repository.AddAll(); err != nil {
+					// 		return errs.WithE(err, "failed to add changes to git")
+					// 	}
 
-					run := runner.NewLocalRunner()
-					return run.ExecCmd("nixos-rebuild", action, "--flake", buildFlakeTarget(bcl.BCL.System.Repository, systemName))
-				} else if err != nil {
-					return errs.WithE(err, "failed to open git repository")
+					// 	run, err := runner.NewSudoRunner(runner.Runner(runner.NewLocalRunner()))
+					// 	if err != nil {
+					// 		return err
+					// 	}
+					// 	//if os.Geteuid() != 0 {
+					// 	// Running sudo internally to prevent root modification of git state during add
+					// 	//var password *memguarded.Service
+					// 	//if err := runner.IsSudoRunnableWithoutPassword(run); err != nil {
+					// 	//	password = memguarded.NewService()
+					// 	//	if err := password.AskSecret(false, "Sudo password to run upgrade"); err != nil {
+					// 	//		return errs.WithE(err, "Failed to get sudo password")
+					// 	//	}
+					// 	//}
+					// 	//run, err = runner.NewSudoRunner(run)
+					// 	//if err != nil {
+					// 	//	return errs.WithE(err, "Failed to create sudo runner")
+					// 	//}
+					// 	//}
+
+					// 	return run.ExecCmd("nixos-rebuild", action, "--flake", buildFlakeTarget(filepath.Join(repository.Root, "nixos"), systemName))
+					return errs.With("not implemented")
 				} else {
-					// Nix build process only files that are in git state
-					if err := repository.AddAll(); err != nil {
-						return errs.WithE(err, "failed to add changes to git")
+					logs.WithField("repo", bcl.BCL.System.Repository).Info("Update current system using upstream state")
+
+					var run runner.Runner = runner.NewLocalRunner()
+					if os.Geteuid() != 0 {
+						sudoRun, err := runner.NewSudoRunner(run)
+						if err != nil {
+							return errs.WithE(err, "Failed to create sudo runner")
+						}
+						run = sudoRun
 					}
 
-					run, err := runner.NewSudoRunner(runner.Runner(runner.NewLocalRunner()))
-					if err != nil {
-						return err
-					}
-					//if os.Geteuid() != 0 {
-					// Running sudo internally to prevent root modification of git state during add
-					//var password *memguarded.Service
-					//if err := runner.IsSudoRunnableWithoutPassword(run); err != nil {
-					//	password = memguarded.NewService()
-					//	if err := password.AskSecret(false, "Sudo password to run upgrade"); err != nil {
-					//		return errs.WithE(err, "Failed to get sudo password")
-					//	}
-					//}
-					//run, err = runner.NewSudoRunner(run)
-					//if err != nil {
-					//	return errs.WithE(err, "Failed to create sudo runner")
-					//}
-					//}
-
-					return run.ExecCmd("nixos-rebuild", action, "--flake", buildFlakeTarget(filepath.Join(repository.Root, "nixos"), systemName))
+					return run.ExecCmd("nixos-rebuild", action, "--flake", buildFlakeTarget(bcl.BCL.System.Repository, systemName))
 				}
 
 			}
@@ -115,6 +122,7 @@ func nixosUpgradeCmd() *cobra.Command {
 
 	cmd.Flags().StringVarP(&action, "action", "a", "switch", "nixos-rebuild action to perform (switch, boot, test, build)")
 	cmd.Flags().StringVarP(&systemName, "system-name", "n", "", "nixos system name to apply. If not provided, will use the current system name. Useful when renaming system")
+	cmd.Flags().BoolVar(&useLocalGitRepository, "git", false, "use current local git repository state instead of upstream repository")
 	withSSHRemoteFlags(cmd, &sshConfig)
 
 	// nixos-rebuild test --refresh --flake git+ssh://git@gitea.lmr.io/lmr/infra?dir=nixos#vm --upgrade
